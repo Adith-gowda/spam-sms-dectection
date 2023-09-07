9ace0cba92d74a04641d287f5d4e65822895605d
Adith sagar gowda <95766897+Adith-gowda@users.noreply.github.com>
2023-09-07 23:38:56 +0530

    Update index.py

--- index.py
+++ index.py
@@ -53,4 +53,4 @@ def result():
     #     return render_template("index.html",p=prediction[0])
 
 if __name__ == "__main__":
-    app.run(debug=True,port=5001)
+    app.run(debug=False,host='0.0.0.0')
